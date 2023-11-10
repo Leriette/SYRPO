63f1344c0abcf88123c2952dae9170cb89f34013
Leriette <nazarevich_lerika@mail.ru>
2023-11-10 15:00:00 +0300

    удалила фцнкционал

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -4,40 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Калькулятор_main");
+        Console.WriteLine("Калькулятор_third");
 
-        while (true)
-        {
-            Console.Write("Введите выражение (или 'exit' для выхода): ");
-            string input = Console.ReadLine();
-
-            if (input.ToLower() == "exit")
-                break;
-
-            try
-            {
-                double result = Evaluate(input);
-                Console.WriteLine($"Результат: {result}");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Ошибка: Некорректный формат ввода.");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Ошибка: Деление на ноль.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка: {ex.Message}");
-            }
-        }
-
-        Console.WriteLine("Работа завершена.");
-    }
-
-    static double Evaluate(string expression)
-    {
-        return Convert.ToDouble(new System.Data.DataTable().Compute(expression, null));
-    }
+    } 
 }
