3df81c809ba5d5b1dcb92640f967be505023c278
Leriette <nazarevich_lerika@mail.ru>
2023-11-17 13:21:37 +0300

    Коммит с ошибкой

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -4,37 +4,9 @@ class Program
 {
     static void Main()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+        int a=2, b=3;
         Console.WriteLine("Калькулятор");
->>>>>>> a66b24e1df527dbeae56eb738537478dc5165e42
-
-<<<<<<< HEAD
-        while (true)
-        {
-            Console.Write("Введите выражение (или 'exit' для выхода): ");
-            string input = Console.ReadLine();
-
-            if (input.ToLower() == "exit")
-                break;
-
-           
-        }
-
-        Console.WriteLine("Работа завершена.");
+        int d = a + b;
+        Console.WriteLine(5 / 0);
     }
-
-    static double Evaluate(string expression)
-    {
-        return Convert.ToDouble(new System.Data.DataTable().Compute(expression, null));
-    }
-=======
-        Console.WriteLine("Калькулятор_2");
-
-     
-    }
-
-   
->>>>>>> 7ba79dc39552f2ea4d8468678fb457b5b34ae7c7
 }
