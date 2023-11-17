60b4e3d4534b28d6ac957e6ae0a60c23c78fa1e6
Leriette <nazarevich_lerika@mail.ru>
2023-11-17 11:48:15 +0300

    Удаление функционала

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -6,38 +6,8 @@ class Program
     {
         Console.WriteLine("Калькулятор");
 
-        while (true)
-        {
-            Console.Write("Введите выражение (или 'exit' для выхода): ");
-            string input = Console.ReadLine();
-
-            if (input.ToLower() == "exit")
-                break;
-
-           
-        }
-
-        Console.WriteLine("Работа завершена");
+     
     }
 
-    static double Evaluate(string expression)
-    {
-        return Convert.ToDouble(new System.Data.DataTable().Compute(expression, null));
-    }
-            {
-                Console.WriteLine("Ошибка: Деление на ноль.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка: {ex.Message}");
-            }
-        }
-
-        Console.WriteLine("Работа завершена.");
-    }
-
-    static double Evaluate(string expression)
-    {
-        return Convert.ToDouble(new System.Data.DataTable().Compute(expression, null));
-    }
+   
 }
