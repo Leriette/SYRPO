12934ad5662fe504e0ced05e5a35d80d610172a7
Leriette <nazarevich_lerika@mail.ru>
2023-11-23 20:53:21 +0300

    a=3

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        int a=2, b=3, c=0;
+        int a=3, b=3, c=0;
         Console.WriteLine("Калькулятор");
         int d = a + b+c;
         Console.WriteLine(d/c);
