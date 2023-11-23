12ee7fd917c86c311d2801047ef2ebf81256e9f8
Leriette <nazarevich_lerika@mail.ru>
2023-11-23 20:42:05 +0300

    Последний коммит

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main()
     {
-        int a=2, b=3;
+        int a=2, b=3, с=0;
         Console.WriteLine("Калькулятор");
-        int d = a + b;
+        int d = a + b+с;
         Console.WriteLine(5/0);
     }
 }
