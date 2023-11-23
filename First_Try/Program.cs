7fd2b3a002871aa22992cc4fb82affb0a610843c
Leriette <nazarevich_lerika@mail.ru>
2023-11-23 20:52:51 +0300

    Убрала +4

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         int a=2, b=3, c=0;
         Console.WriteLine("Калькулятор");
-        int d = a + b+c+4;
+        int d = a + b+c;
         Console.WriteLine(d/c);
     }
 }
