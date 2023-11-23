82b41a80ddb38034e222c7bbd40fda506856a9fc
Leriette <nazarevich_lerika@mail.ru>
2023-11-23 20:50:44 +0300

    Все работает

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -7,6 +7,6 @@ class Program
         int a=2, b=3, с=0;
         Console.WriteLine("Калькулятор");
         int d = a + b+с;
-        Console.WriteLine(5/0);
+        Console.WriteLine(d);
     }
 }
