6c55b5dff2c923fad3fd16ebd3c7fbdad26313c9
Leriette <nazarevich_lerika@mail.ru>
2023-11-23 20:59:42 +0300

    Update Program.cs

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -7,6 +7,6 @@ class Program
         int a=3, b=3, c=0;
         Console.WriteLine("Калькулятор");
         int d = a + b+c;
-        Console.WriteLine(d/c);
+        Console.WriteLine(d);
     }
 }
