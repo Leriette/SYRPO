bf66d2a25452ceaca3246cf323d2c26c177694c0
Leriette <nazarevich_lerika@mail.ru>
2023-11-23 20:39:16 +0300

    Добавлена c

--- First_Try/Program.cs
+++ First_Try/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main()
     {
-        int a=2, b=3;
+        int a=2, b=3, c=0;
         Console.WriteLine("Калькулятор");
         int d = a + b;
-        Console.WriteLine(d);
+        Console.WriteLine(d+c);
     }
 }
